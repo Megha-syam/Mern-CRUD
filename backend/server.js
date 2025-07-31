5fa02677531b80d25f228c4f1fe36d7d334ba3ff
Megha Syam Kambhampati <2200033128@kluniversity.in>
2025-07-31 13:07:03 +0530

    updated server.js

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const cors=require('cors');
+const studentRoutes=require('./routes/student.routes');
+
+const app=express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/students',studentRoutes);
+
+mongoose.connect('mongodb://127.0.0.1:27017/studentDB',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+.then(()=>{
+    console.log('Connected to MongoDB');
+
+    app.listen(5000,()=>{
+        console.log('Server running on port 5000');
+    });
+})
+.catch((err)=>{
+    console.log('Failed to connect MongoDB',err);
+});
